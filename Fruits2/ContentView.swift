bb8c6bcedcda9cdbdc7037b017f287c781fc6d14
Jonson5454 <asgsdsdsa@gmail.com>
2021-10-14 15:36:28 +0500

    Initial Commit

---
+++ Fruits2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fruits2
+//
+//  Created by a on 10/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fruits2/Fruits2App.swift
@@ -0,0 +1,17 @@
+//
+//  Fruits2App.swift
+//  Fruits2
+//
+//  Created by a on 10/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Fruits2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
